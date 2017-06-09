4c4d32584ba743c3bf152fc7dc618b1fcd38bb5d
Błażej Cegiełka <blazej.cegielka@gmail.com>
2017-06-09 12:03:07 +0100

    Added vcr use_cassette context wrapper

--- nflex_connector_utils/tools.py
+++ nflex_connector_utils/tools.py
@@ -1,3 +1,6 @@
+import os
+
+
 def serialize_list(data):
     """
         Convert all objects in a list to the CMP data structure by calling
@@ -11,3 +14,25 @@ def serialize_list(data):
     """
 
     return [d.serialize() for d in data]
+
+
+def vcr_cassette_context(function):
+    """
+        Enclose decorated function into vcr.use_cassette context.
+
+        Args:
+            function (function): inner function to enclose
+
+        Returns:
+            function: wrapper of inner function
+    """
+    def wrapper(*args, **kwargs):
+        if 'DEBUG_VCR' in os.environ:
+            import vcr
+            with vcr.use_cassette('vcr.yaml', record_mode='new_episodes',
+                                  ignore_hosts=['192.168.42.24']):
+                return function(*args, **kwargs)
+        else:
+            return function(*args, **kwargs)
+
+    return wrapper
